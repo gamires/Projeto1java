ecfb7fc01c4eb50b8b24828d2f0abb8a6a4ababa
gamires <gabi.mires.games@gmail.com>
2025-03-14 12:08:39 +0000

    mires_vars1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        try{
-            int a = 0;
-        }
+    Scanner scanner = new scanner(System.in);
+    ArrayList<String> equaArrayList = new ArrayList<>();
+    char[] entrada = new char[100];
+    System.out.println("Digite: ");
+        while(entrada != "exit")
+            try{
+                Scanner entrada = scanner.nextLine();
+                //Y = 3
+                entrada = entrada.replacelAll(" ", "");
+                //Y=3
+                for(int i = 0;i<alfabeto.length;i++){
+                    if(entrada[0] == alfabeto[i]){
+                        
+                    }
+                }
+                if(entrada[0] = entrada.CharAt){
+                // analisar primero termo se termo = a uma letra do alfabeto
+
+                }
+                    equaArrayList.add();
+            }
 
         catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
